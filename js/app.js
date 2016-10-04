c14471d48d7d45a6b80c6ea7c80aca570425c1c7
chelseadlee <chelsea.d.lee12@gmail.com>
2016-10-04 15:15:55 -0700

    add game alerts and readme

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-// Enemies our player must avoid
 var CANVAS_WIDTH = 505;
 
-var Enemy = function(x,y) {
+/*************** Enemy section ***************/
+// Enemies our player must avoid
+var Enemy = function(x, y) {
     this.startingX = x;
     this.x = x;
     this.y = y;
-    this.speed = Math.floor(Math.random() * 70) + 30;
+    this.speed = 0;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+};
+
+// Set enemy speed to random number
+Enemy.prototype.setSpeed = function(min, max) {
+    this.speed = Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Update the enemy's position, required method for game
@@ -23,108 +29,147 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-var setMode = function() {
-    if (score < 5) {
-        currentMode.easy();
-    } else if (score < 10) {
-        currentMode.medium();
-    } else if (score < 20) {
-        setMode();
-    } else if (score === 20) {
-        console.log("you win!");
-    }
-};
-
-var Mode = function(){
-    var enemy1 = new Enemy(0,230);
-
-    var enemy2 = new Enemy(0,145);
-
-    var enemy3 = new Enemy(-40,145);
-
-    var enemy4 = new Enemy(-180,60);
-
-    var enemy5 = new Enemy(-200,230);
-
-    var enemy6 = new Enemy(-20,145);
-
-    var enemy7 = new Enemy(-90,145);
-
-    var enemy8 = new Enemy(-280,60);
-
-    this.easyEnemies = [enemy1, enemy2, enemy3, enemy4];
-    this.mediumEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8];
-
-    this.allEnemies = [];
-};
-
-Mode.prototype.easy = function(){
-    this.allEnemies = this.easyEnemies;
-};
-
-Mode.prototype.medium = function(){
-    this.allEnemies = this.mediumEnemies;
-};
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Reset enemy to starting x position
 Enemy.prototype.reset = function() {
     this.x = this.startingX;
 }
 
-/*************** player section ***************/
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x,y) {
+/*************** Mode section ***************/
+
+var score = 0;
+
+// Set mode (easy, medium, hard) depending on score and display appropriate alert message
+var setMode = function() {
+    var currentDifficulty = currentMode.difficultyLevel;
+    if (score < 5) {
+        currentMode.easy();
+    } else if (score < 10) {
+        currentMode.medium();
+        if (currentDifficulty !== currentMode.difficultyLevel) {
+            alert("level up!");
+        }
+    } else if (score < 20) {
+        currentMode.hard();
+        if (currentDifficulty !== currentMode.difficultyLevel) {
+            alert("level up!");
+        }
+    } else if (score === 20) {
+        alert("YOU WIN!!");
+    }
+};
+
+var Mode = function() {
+    this.difficultyLevel = null;
+
+    // Instantiate enemies
+    var enemy1 = new Enemy(0, 230);
+
+    var enemy2 = new Enemy(0, 145);
+
+    var enemy3 = new Enemy(-40, 145);
+
+    var enemy4 = new Enemy(-180, 60);
+
+    var enemy5 = new Enemy(-200, 230);
+
+    var enemy6 = new Enemy(-20, 145);
+
+    var enemy7 = new Enemy(-90, 230);
+
+    var enemy8 = new Enemy(-280, 60);
+
+    // Set number of enemies for easy, medium and hard modes
+    this.easyEnemies = [enemy1, enemy2, enemy3, enemy4];
+    this.mediumEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+    this.hardEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8];
+    this.allEnemies = [];
+};
+
+Mode.prototype.easy = function() {
+    this.difficultyLevel = "easy";
+    this.allEnemies = this.easyEnemies;
+
+    // Set speed of enemies in easy mode
+    this.allEnemies.forEach(function(enemy) {
+        enemy.setSpeed(30, 80);
+    })
+};
+
+Mode.prototype.medium = function() {
+    this.difficultyLevel = "medium";
+    this.allEnemies = this.mediumEnemies;
+
+    // Set speed of enemies in medium mode
+    this.allEnemies.forEach(function(enemy) {
+        enemy.setSpeed(70, 120);
+    })
+};
+
+Mode.prototype.hard = function() {
+    this.difficultyLevel = "hard";
+    this.allEnemies = this.hardEnemies;
+
+    // Set speed of enemies in hard mode
+    this.allEnemies.forEach(function(enemy) {
+        enemy.setSpeed(100, 140);
+    })
+};
+
+/*************** Player section ***************/
+var Player = function(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-cat-girl.png';
 };
 
+// Reset player starting position
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 435;
 };
 
+// Check to see if player makes it to water
 Player.prototype.checkWin = function() {
     if (this.y <= -10) {
         this.reset();
-        console.log("you win!");
-        score ++;
+        score++;
+        setMode();
         console.log(score);
     }
 };
 
-Player.prototype.update = function(){
-    setMode();
+Player.prototype.update = function() {
     this.checkWin();
     this.handleInput();
     this.checkCollisions();
-   };
+};
 
-Player.prototype.handleInput = function(direction){
+Player.prototype.handleInput = function(direction) {
     var movement = 50;
 
-    switch(direction) {
-        case 'left' :
+    switch (direction) {
+        case 'left':
             this.x = this.x - movement > 0 ? this.x - movement : -10;
             break;
-        case 'right' :
+        case 'right':
             this.x = this.x + movement < 410 ? this.x + movement : 410;
             break;
-        case 'down' :
-            this.y = this.y + movement < 435 ? this.y +  movement : 435;
+        case 'down':
+            this.y = this.y + movement < 435 ? this.y + movement : 435;
             break;
-        case 'up' :
+        case 'up':
             this.y = this.y - movement > -10 ? this.y - movement : -10;
             break;
     }
 };
 
-Player.prototype.checkCollisions = function(){
+// Check player's position against all enemies
+Player.prototype.checkCollisions = function() {
 
     for (enemy = 0; enemy < 4; enemy++) {
         var playerHeight = 50;
@@ -135,30 +180,28 @@ Player.prototype.checkCollisions = function(){
             currentMode.allEnemies[enemy].x + spriteWidth > player.x &&
             currentMode.allEnemies[enemy].y < player.y + playerHeight &&
             spriteHeight + currentMode.allEnemies[enemy].y > player.y) {
-                console.log("collision");
-                player.reset();
-                score --;
-                console.log(score);
+            console.log("collision");
+            player.reset();
+
+            // Penalty: score is reduced
+            score--;
+            setMode();
+            console.log(score);
         }
     }
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    // Score text
     ctx.font = "40px Arial";
     ctx.fillStyle = "black";
-    ctx.fillText("Score: "+score, 0, 580);
+    ctx.fillText("Score: " + score, 0, 580);
 };
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
+// Instantiate Player object
 var player = new Player(200, 435);
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -172,7 +215,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-// keep score and level up
-
-var score = 0;
